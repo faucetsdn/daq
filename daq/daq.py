158d058e7e0d50cd5d03baf0f7e56e819393ff51
Trevor Pering <peringknife@google.com>
2018-03-16 21:07:19 -0700

    Adding add/remove host code/test

--- daq/daq.py
+++ daq/daq.py
@@ -6,42 +6,66 @@ from __future__ import print_function
 
 import logging
 import os
+import time
 
-from mininet import log as minilogger
+from mininet import log as minilog
 from mininet.net import Mininet
-from mininet.node import Controller, RemoteController
+from mininet.node import Host
 from mininet.cli import CLI
 from mininet.node import OVSBridge
 
 
-from tests.faucet_mininet_test_topo import FaucetHost, FaucetSwitch
+from tests.faucet_mininet_test_topo import FaucetHostCleanup
 
 logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger( __name__ )
+logger = logging.getLogger(__name__)
+
+
+class DAQHost(FaucetHostCleanup, Host):
+    """Base Mininet Host class, for Mininet-based tests."""
+
+    pass
+
+
+def addHost(net, switch, name):
+    host = net.addHost(name, cls=DAQHost)
+    link = net.addLink(host, switch)
+    if net.built:
+        host.configDefault()
+        switch.attach(link.intf2.name)
+    return host
+
 
 def createNetwork():
     logging.debug("Creating miniet...")
     net = Mininet()
 
     logging.debug("Adding hosts...")
-    h1 = net.addHost('h1', cls=FaucetHost)
-    h2 = net.addHost('h2', cls=FaucetHost)
-
     logging.debug("Adding switch and controller...")
-    s1 = net.addSwitch('s1', cls=OVSBridge)
+    switch = net.addSwitch('s1', cls=OVSBridge)
 
-    logging.debug("Adding links...")
-    net.addLink(h1, s1)
-    net.addLink(h2, s1)
+    h1 = addHost(net, switch, 'h1')
+    h2 = addHost(net, switch, 'h2')
     
     logging.debug("Starting mininet...")
     net.start()
 
     logging.debug("Ping test h1->h2")
-    print(h1.cmd( 'ping -c1', h2.IP(), '> /dev/null || echo ping FAILED' ), end='')
+    print(h1.cmd('ping -c1', h2.IP(), '> /dev/null || echo ping FAILED'), end='')
     logging.debug("Ping test h2->h1")
-    print(h2.cmd( 'ping -c1', h1.IP(), '> /dev/null || echo ping FAILED' ), end='')
+    print(h2.cmd('ping -c1', h1.IP(), '> /dev/null || echo ping FAILED'), end='')
+
+    h3 = addHost(net, switch, 'h3')
+    h2.terminate()
+    time.sleep(1)
     
+    logging.debug("Ping test h1->h2 (should fail)")
+    print(h1.cmd('ping -c1', h2.IP(), '> /dev/null || echo ping FAILED'), end='')
+    logging.debug("Ping test h1->h3")
+    print(h1.cmd('ping -c1', h3.IP(), '> /dev/null || echo ping FAILED'), end='')
+    logging.debug("Ping test h3->h1")
+    print(h3.cmd('ping -c1', h1.IP(), '> /dev/null || echo ping FAILED'), end='')
+
     CLI(net)
 
     logging.debug("Stopping mininet...")
@@ -50,7 +74,7 @@ def createNetwork():
 
 
 if __name__ == '__main__':
-    minilogger.setLogLevel('info')
+    minilog.setLogLevel('info')
     if os.getuid() != 0:
         logger.debug("You are NOT root")
     elif os.getuid() == 0:
