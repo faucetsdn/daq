7ee581da5e22ed3397a16b7250d6632dba501a20
Trevor Pering <peringknife@google.com>
2018-03-22 17:43:56 -0700

    Working faux-device setup

--- daq/daq.py
+++ daq/daq.py
@@ -26,15 +26,18 @@ class DAQHost(FaucetHostCleanup, Host):
     pass
 
 
-def addHost(net, switch, name, cls=DAQHost, ip=None):
+def addHost(net, switch, name, cls=DAQHost, ip=None, volumes=[]):
+    tmpdir = 'inst/'
     params = { 'ip': ip } if ip else {}
-    params['tmpdir'] = 'inst/'
+    params['tmpdir'] = tmpdir
+    params['volumes'] = volumes
     host = net.addHost(name, cls, **params)
     link = net.addLink(switch, host, fast=False)
     if net.built:
         host.configDefault()
         switch.attach(link.intf2.name)
     return host
+
 
 def stopHost():
     logging.debug("Stopping host h2")
@@ -63,13 +66,17 @@ def createNetwork():
     logging.debug("Adding hosts...")
     h1 = addHost(net, switch, 'h1', cls=MakeFaucetDockerHost('daq/networking'))
     h2 = addHost(net, switch, 'h2', cls=MakeFaucetDockerHost('daq/fauxdevice'), ip="0.0.0.0")
-    h3 = addHost(net, switch, 'h3', cls=MakeFaucetDockerHost('daq/default'))
+
+    # No real point to this just yet, except to test volume mappings.
+    volumes = [ ('build', '/root') ]
+    h3 = addHost(net, switch, 'h3', cls=MakeFaucetDockerHost('daq/default'), volumes=volumes)
 
     logging.debug("Starting mininet...")
     net.start()
 
     h1.activate()
     h2.activate()
+    h3.activate()
 
     logging.debug("Waiting for system to settle...")
     time.sleep(4)
@@ -82,6 +89,15 @@ def createNetwork():
     print(h1.cmd('ping -c1', h3.IP(), '> /dev/null || echo ping FAILED'), end='')
     logging.debug("Ping test h3->h1")
     print(h3.cmd('ping -c1', h1.IP(), '> /dev/null || echo ping FAILED'), end='')
+
+    logging.debug("Waiting for dhcp...")
+    time.sleep(2)
+
+    logging.debug("Ping test h1->h2")
+    print(h1.cmd('ping -c1', h2.IP(), '> /dev/null || echo ping FAILED'), end='')
+    logging.debug("Ping test h2->h1")
+    print(h2.cmd('ping -c1', h1.IP(), '> /dev/null || echo ping FAILED'), end='')
+
 
     CLI(net)
 
---
+++ misc/fauxdevice.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dhclient
+
+tail -f /dev/null
