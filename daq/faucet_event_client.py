141573cae58d2fdcd6b02766f2270db84e001214
Trevor Pering <peringknife@google.com>
2018-03-23 08:52:01 -0700

    Adding image export/import to build

--- daq/faucet_event_client.py
+++ daq/faucet_event_client.py
@@ -6,13 +6,15 @@ import time
 class FaucetEventClient():
     """A general client interface to the FAUCET event API"""
 
+    FAUCET_RETRIES=5
+
     sock = None
     buffer = b''
 
     def connect(self, sock_path):
         """Make connection to sock to receive events"""
 
-        retries = 3
+        retries = self.FAUCET_RETRIES
         while not os.path.exists(sock_path):
             assert retries > 0, "Could not find socket path %s" % sock_path
             retries -= 1
@@ -39,7 +41,7 @@ class FaucetEventClient():
         if event['PORT_CHANGE']['reason'] != "ADD":
             return None
         return event['PORT_CHANGE']['port_no']
-        
+
     def close(self):
         self.sock.close()
         self.sock = None
