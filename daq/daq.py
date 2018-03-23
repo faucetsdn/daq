7f398ae18bdf9bebd6f1ff543d41cb7114f3ce6d
Trevor Pering <peringknife@google.com>
2018-03-22 21:29:42 -0700

    Convert DAQRunner to proper class

--- daq/daq.py
+++ daq/daq.py
@@ -24,94 +24,94 @@ class DAQHost(FaucetHostCleanup, Host):
     pass
 
 
-def addHost(net, switch, name, cls=DAQHost, ip=None, env_vars=[]):
-    tmpdir = 'inst/'
-    params = { 'ip': ip } if ip else {}
-    params['tmpdir'] = tmpdir
-    params['env_vars'] = env_vars
-    host = net.addHost(name, cls, **params)
-    link = net.addLink(switch, host, fast=False)
-    if net.built:
-        host.configDefault()
-        switch.attach(link.intf1.name)
-    return host
+class DAQRunner():
+    net = None
+    switch = None
 
+    def addHost(self, name, cls=DAQHost, ip=None, env_vars=[]):
+        tmpdir = 'inst/'
+        params = { 'ip': ip } if ip else {}
+        params['tmpdir'] = tmpdir
+        params['env_vars'] = env_vars
+        host = self.net.addHost(name, cls, **params)
+        link = self.net.addLink(self.switch, host, fast=False)
+        if self.net.built:
+            host.configDefault()
+            self.switch.attach(link.intf1.name)
+        return host
 
-def stopHost():
-    logging.debug("Stopping host h2")
-    h2.terminate()
-    time.sleep(1)
+    def stopHost(self, host):
+        logging.debug("Stopping host " + host.name)
+        host.terminate()
 
+    def pingTest(self, a, b):
+        logging.debug("Ping test %s->%s" % (a.name, b.name))
+        failure="ping FAILED"
+        assert b.IP() != "0.0.0.0", "IP address not assigned, can't ping"
+        output = a.cmd('ping -c1', b.IP(), '> /dev/null 2>&1 || echo ', failure).strip()
+        if output:
+            print output
+        return output.strip() != failure
 
-def pingTest(a, b):
-    logging.debug("Ping test %s->%s" % (a.name, b.name))
-    failure="ping FAILED"
-    assert b.IP() != "0.0.0.0", "IP address not assigned, can't ping"
-    output = a.cmd('ping -c1', b.IP(), '> /dev/null 2>&1 || echo ', failure).strip()
-    if output:
-        print output
-    return output.strip() != failure
+    def createNetwork(self):
+        logging.debug("Creating miniet...")
+        self.net = Mininet()
 
+        logging.debug("Adding switch...")
+        self.switch = self.net.addSwitch('s1', cls=OVSSwitch)
 
-def createNetwork():
+        logging.debug("Starting faucet container...")
+        self.switch.cmd('cmd/faucet')
 
-    logging.debug("Creating miniet...")
-    net = Mininet()
+        targetIp = "127.0.0.1"
+        logging.debug("Adding controller at %s" % targetIp)
+        c1 = self.net.addController( 'c1', controller=RemoteController, ip=targetIp, port=6633 )
 
-    logging.debug("Adding switch...")
-    switch = net.addSwitch('s1', cls=OVSSwitch)
+        logging.debug("Adding hosts...")
+        h1 = self.addHost('h1', cls=MakeFaucetDockerHost('daq/networking'))
+        h2 = self.addHost('h2', cls=MakeFaucetDockerHost('daq/fauxdevice'), ip="0.0.0.0")
+        h3 = self.addHost('h3', cls=MakeFaucetDockerHost('daq/default'))
 
-    logging.debug("Starting faucet container...")
-    switch.cmd('cmd/faucet')
+        logging.debug("Starting mininet...")
+        self.net.start()
 
-    targetIp = "127.0.0.1"
-    logging.debug("Adding controller at %s" % targetIp)
-    c1 = net.addController( 'c1', controller=RemoteController, ip=targetIp, port=6633 )
+        logging.debug("Activating hosts...")
+        h1.activate()
+        h2.activate()
+        h3.activate()
 
-    logging.debug("Adding hosts...")
-    h1 = addHost(net, switch, 'h1', cls=MakeFaucetDockerHost('daq/networking'))
-    h2 = addHost(net, switch, 'h2', cls=MakeFaucetDockerHost('daq/fauxdevice'), ip="0.0.0.0")
-    h3 = addHost(net, switch, 'h3', cls=MakeFaucetDockerHost('daq/default'))
+        logging.debug("Waiting for system to settle...")
+        time.sleep(3)
 
-    logging.debug("Starting mininet...")
-    net.start()
+        self.pingTest(h1, h3)
+        self.pingTest(h3, h1)
 
-    h1.activate()
-    h2.activate()
-    h3.activate()
+        if self.pingTest(h2, h1):
+            print "Unexpected success??!?!"
+        else:
+            print "(Expected failure)"
 
-    logging.debug("Waiting for system to settle...")
-    time.sleep(3)
+        logging.debug("Waiting for dhcp...")
+        time.sleep(5)
 
-    pingTest(h1, h3)
-    pingTest(h3, h1)
+        self.pingTest(h2, h1)
 
-    if pingTest(h2, h1):
-        print "Unexpected success??!?!"
-    else:
-        print "(Expected failure)"
+        logging.debug("Creating/activating test_ping")
+        env_vars = [ "TARGET_HOST=" + h1.IP() ]
+        h4 = self.addHost('h4', cls=MakeFaucetDockerHost('daq/test_ping'), env_vars = env_vars)
+        h4.activate()
 
-    logging.debug("Waiting for dhcp...")
-    time.sleep(5)
+        CLI(self.net)
 
-    pingTest(h2, h1)
-
-    logging.debug("Creating/activating test_ping")
-    env_vars = [ "TARGET_HOST=" + h1.IP() ]
-    h4 = addHost(net, switch, 'h4', cls=MakeFaucetDockerHost('daq/test_ping'), env_vars = env_vars)
-    h4.activate()
-
-    CLI(net)
-
-    logging.debug("Stopping faucet...")
-    switch.cmd('docker kill daq-faucet')
-    logging.debug("Stopping mininet...")
-    net.stop()
+        logging.debug("Stopping faucet...")
+        self.switch.cmd('docker kill daq-faucet')
+        logging.debug("Stopping mininet...")
+        self.net.stop()
 
 
 if __name__ == '__main__':
     minilog.setLogLevel('info')
-    if os.getuid() != 0:
+    if os.getuid() == 0:
+        DAQRunner().createNetwork()
+    else:
         logger.debug("You are NOT root")
-    elif os.getuid() == 0:
-        createNetwork()
