c28cf9d9555c40a2858b86bb46261fb6a2916f37
Trevor Pering <peringknife@google.com>
2018-04-19 09:09:43 -0700

    Properly process ports status initial update

--- daq/faucet_event_client.py
+++ daq/faucet_event_client.py
@@ -39,29 +39,62 @@ class FaucetEventClient():
         while True:
             if '\n' in self.buffer:
                 return True
-            elif self.has_data() or blocking:
+            elif blocking or self.has_data():
                 self.buffer += self.sock.recv(1024)
             else:
                 return False
 
-    def next_event(self):
-        while True:
-            if self.has_event(blocking=True):
-                line, remainder = self.buffer.split('\n', 1)
-                self.buffer = remainder
-                return json.loads(line)
+    def filter_state_update(self, event):
+        (dpid, port, active) = self.as_port_state(event)
+        if dpid and port:
+            state_key = '%s-%d' % (dpid, port)
+            if state_key in self.previous_state and self.previous_state[state_key] == active:
+                return None
+            self.previous_state[state_key] = active
+            return event
+
+        (dpid, states) = self.as_ports_status(event)
+        if dpid:
+            for port in states:
+                self.prepend_event(self.make_port_state(dpid, port, states[port]))
+            return None
+        return event
+
+    def prepend_event(self, event):
+        self.buffer = '%s\n%s' % (json.dumps(event), self.buffer)
+
+    def next_event(self, blocking=False):
+        while self.has_event(blocking=blocking):
+            line, remainder = self.buffer.split('\n', 1)
+            self.buffer = remainder
+            event = json.loads(line)
+            event = self.filter_state_update(event)
+            if event:
+                return event
+        return None
+
+    def as_ports_status(self, event):
+        if not 'PORTS_STATUS' in event:
+            return (None, None)
+        return (event['dp_id'], event['PORTS_STATUS'])
+
+    def make_port_state(self, dpid, port, state):
+        port_change = {}
+        port_change['port_no'] = port
+        port_change['status'] = state
+        port_change['reason'] = 'MODIFY'
+        event = {}
+        event['dp_id'] = dpid
+        event['PORT_CHANGE'] = port_change
+        return event
 
     def as_port_state(self, event):
         if not 'PORT_CHANGE' in event:
             return (None, None, None)
         dpid = event['dp_id']
         reason = event['PORT_CHANGE']['reason']
-        port_no = event['PORT_CHANGE']['port_no']
+        port_no = int(event['PORT_CHANGE']['port_no'])
         port_active = event['PORT_CHANGE']['status'] and not reason == 'DELETE'
-        state_key = '%s-%d' % (dpid, port_no)
-        if state_key in self.previous_state and self.previous_state[state_key] == port_active:
-            return (None, None, None)
-        self.previous_state[state_key] = port_active
         return (dpid, port_no, port_active)
 
     def close(self):
