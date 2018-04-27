f7d053872e9e1f6109a69b85ac7171a620c3b177
Trevor Pering <peringknife@google.com>
2018-04-27 15:26:11 -0700

    Delete records more than 24hr old

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,6 +1,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
+const expiry_ms = 1000 * 60 * 60 * 24;
+
 admin.initializeApp(functions.config().firebase);
 var db = admin.firestore();
 
@@ -9,16 +11,27 @@ exports.daq_firestore = functions.pubsub.topic('daq_runner').onPublish((event) =
   const origin = event.attributes.origin;
   const port = 'port-' + message.port;
   const timestr = new Date().toTimeString();
-  message.updated = timestr;
+  const timestamp = Date.now();
+  const expired = timestamp - expiry_ms;
 
   const origin_doc = db.collection('origin').doc(origin);
   origin_doc.set({'updated': timestr});
   const port_doc = origin_doc.collection('port').doc(port);
   port_doc.set({'updated': timestr});
   const run_doc = port_doc.collection('runid').doc(message.runid);
-  run_doc.set({'updated': timestr});
+  run_doc.set({'updated': timestr, 'timestamp': timestamp});
   const test_doc = run_doc.collection('test').doc(message.name);
   test_doc.set(message);
+
+  port_doc.collection('runid').where('timestamp', '<', expired)
+    .get().then(function(snapshot) {
+      snapshot.forEach(function(old_doc) {
+        const del_doc = port_doc.collection('runid').doc(old_doc.id);
+        del_doc.delete().catch((error) => {
+          console.error('Error deleting doc ', old_doc.id, error);
+        });
+      });
+    });
   
   return null;
 });
