1501d6f4ba3e252839fd016e6b7513fca47ef0ee
Trevor Pering <peringknife@google.com>
2018-04-26 22:00:01 -0700

    Update to use timestamps for database

--- cloud/functions/index.js
+++ cloud/functions/index.js
@@ -9,6 +9,17 @@ exports.daq_firestore = functions.pubsub.topic('daq_runner').onPublish((event) =
   const origin = event.attributes.origin;
   const port = 'port-' + message.port;
   const test = message.name;
-  db.collection('origin').doc(origin).collection('port').doc(port).collection('test').doc(test).set(message);
+  const timestamp = String(Date.now());
+  const timestr = new Date().toTimeString();
+  message.updated = timestr;
+  port_doc = db
+    .collection('origin').doc(origin)
+    .collection('port').doc(port);
+  port_doc
+    .set({'updated': timestr});
+  port_doc
+    .collection('test').doc(test)
+    .collection('time').doc(timestamp)
+    .set(message);
   return null;
 });
