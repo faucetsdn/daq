27346c055458ae94d6dba80d1fa92c00a2ba4bba
Trevor Pering <peringknife@google.com>
2018-04-30 15:09:42 -0700

    Up DHCP timeout to 30s

--- daq/daq.py
+++ daq/daq.py
@@ -55,7 +55,7 @@ class ConnectedHost():
 
     TEST_IP_FORMAT = '192.168.84.%d'
     MONITOR_SCAN_SEC = 10
-    DHCP_TIMEOUT_SEC = 10
+    DHCP_TIMEOUT_SEC = 30
     IMAGE_NAME_FORMAT = 'daq/test_%s'
     CONTAINER_PREFIX = 'daq'
 
