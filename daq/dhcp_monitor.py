4deaec9b19bce8585010b383ff48f29d31a143ca
Trevor Pering <peringknife@google.com>
2018-06-12 17:20:40 -0700

    Importing DAQ helper modules

---
+++ daq/dhcp_monitor.py
@@ -0,0 +1,85 @@
+"""Encapsulate DHCP monitor/startup"""
+
+import logging
+import re
+import time
+
+from clib.tcpdump_helper import TcpdumpHelper
+
+LOGGER = logging.getLogger('dhcp')
+
+class DhcpMonitor(object):
+    """Class to handle DHCP monitoring"""
+
+    DHCP_MAC_PATTERN = '> ([0-9a-f:]+), ethertype IPv4'
+    DHCP_IP_PATTERN = 'Your-IP ([0-9.]+)'
+    DHCP_PATTERN = '(%s)|(%s)' % (DHCP_MAC_PATTERN, DHCP_IP_PATTERN)
+    DHCP_TIMEOUT_SEC = 240
+    DHCP_THRESHHOLD_SEC = 20
+
+    def __init__(self, runner, port_set, container, callback):
+        self.runner = runner
+        self.port_set = port_set
+        self.callback = callback
+        self.networking = container
+        self.target_ip = None
+        self.target_mac = None
+        self.dhcp_traffic = None
+        self.runner = None
+        self.port_set = None
+        self.intf_name = None
+        self.callback = None
+        self.test_start = None
+
+    def start(self):
+        """Start monitoring DHCP"""
+        LOGGER.info('Set %d waiting for dhcp reply from %s...', self.port_set, self.networking.name)
+        self.test_start = int(time.time())
+        tcp_filter = "src port 67"
+        self.dhcp_traffic = TcpdumpHelper(self.networking, tcp_filter, packets=None,
+                                          timeout=self.DHCP_TIMEOUT_SEC, blocking=False)
+        self.runner.monitor_stream(self.networking.name, self.dhcp_traffic.stream(),
+                                   self._dhcp_line, hangup=self._dhcp_hangup,
+                                   error=self._dhcp_error)
+
+    def _dhcp_line(self):
+        dhcp_line = self.dhcp_traffic.next_line()
+        if not dhcp_line:
+            return
+        match = re.search(self.DHCP_PATTERN, dhcp_line)
+        if match:
+            self.target_ip = match.group(4)
+            if self.target_ip:
+                message = 'dhcp IP %s found, but no MAC address: %s' % (self.target_ip, dhcp_line)
+                assert self.target_mac, message
+                self._dhcp_success()
+            else:
+                self.target_mac = match.group(2)
+
+    def cleanup(self, forget=True):
+        """Cleanup any ongoing dhcp activity"""
+        if self.dhcp_traffic:
+            if forget:
+                self.runner.monitor_forget(self.dhcp_traffic.stream())
+            self.dhcp_traffic.terminate()
+            self.dhcp_traffic = None
+
+    def _dhcp_success(self):
+        self.cleanup()
+        delta = int(time.time()) - self.test_start
+        LOGGER.info('Set %d received dhcp reply after %ds: %s is at %s',
+                    self.port_set, delta, self.target_mac, self.target_ip)
+        weak_result = delta > self.DHCP_THRESHHOLD_SEC
+        state = 'weak' if weak_result else None
+        self.callback(state, target_mac=self.target_mac, target_ip=self.target_ip)
+
+    def _dhcp_hangup(self):
+        try:
+            raise Exception('dhcp hangup')
+        except Exception as e:
+            self._dhcp_error(e)
+
+    def _dhcp_error(self, e):
+        LOGGER.error('Set %d dhcp error: %s', self.port_set, e)
+        self.cleanup(forget=False)
+        self.callback('error', exception=e)
---
+++ daq/network.py
@@ -0,0 +1,182 @@
+"""Networking module"""
+
+import logging
+import os
+import time
+
+import mininet
+import clib
+
+LOGGER = logging.getLogger('network')
+
+class DAQHost(clib.mininet_test_topo.FaucetHostCleanup, mininet.node.Host):
+    """Base Mininet Host class, for Mininet-based tests."""
+    # pylint: disable=too-few-public-methods
+    pass
+
+
+class DummyNode(object):
+    """Dummy node used to handle shadow devices"""
+    # pylint: disable=invalid-name
+    def addIntf(self, node, port=None):
+        """No-op for adding an interface"""
+        pass
+
+    def cmd(self, cmd, *args, **kwargs):
+        """No-op for running a command"""
+        pass
+
+
+class TestNetwork(object):
+    """Test network manager"""
+
+    OVS_CLS = mininet.node.OVSSwitch
+
+    def __init__(self, config):
+        self.config = config
+        self.net = None
+        self.pri = None
+        self.sec = None
+        self.sec_dpid = None
+        self.sec_port = None
+        self.sec_name = None
+        self.switch_links = {}
+        self.device_intfs = None
+
+    # pylint: disable=too-many-arguments
+    def add_host(self, name, cls=DAQHost, ip_addr=None, env_vars=None, vol_maps=None,
+                 port=None, tmpdir=None):
+        """Add a host to the ecosystem"""
+        params = {'ip': ip_addr} if ip_addr else {}
+        params['tmpdir'] = os.path.join(tmpdir, 'nodes') if tmpdir else None
+        params['env_vars'] = env_vars if env_vars else []
+        params['vol_maps'] = vol_maps if vol_maps else []
+        host = self.net.addHost(name, cls, **params)
+        try:
+            LOGGER.debug('Created host %s with pid %s/%s', name, host.pid, host.shell.pid)
+            switch_link = self.net.addLink(self.pri, host, port1=port, fast=False)
+            self.switch_links[host] = switch_link
+            if self.net.built:
+                host.configDefault()
+                self._switch_attach(self.pri, switch_link.intf1)
+        except:
+            host.terminate()
+            raise
+        return host
+
+    def get_host_interface(self, host):
+        """Get the internal link interface for this host"""
+        return self.switch_links[host].intf2
+
+    def _switch_attach(self, switch, intf):
+        switch.attach(intf)
+        # This really should be done in attach, but currently only automatic on switch startup.
+        switch.vsctl(switch.intfOpts(intf))
+
+    def _switch_del_intf(self, switch, intf):
+        del switch.intfs[switch.ports[intf]]
+        del switch.ports[intf]
+        del switch.nameToIntf[intf.name]
+
+    def remove_host(self, host):
+        """Remove a host from the ecosystem"""
+        index = self.net.hosts.index(host)
+        if index:
+            del self.net.hosts[index]
+        if host in self.switch_links:
+            switch_link = self.switch_links[host]
+            del self.switch_links[host]
+            intf = switch_link.intf1
+            self.pri.detach(intf)
+            self._switch_del_intf(self.pri, intf)
+            intf.delete()
+            del self.net.links[self.net.links.index(switch_link)]
+
+    def _make_device_intfs(self):
+        intf_names = self.config['daq_intf'].split(',')
+        intfs = []
+        for intf_name in intf_names:
+            intf_name = intf_name[0:-1] if intf_name.endswith('!') else intf_name
+            port_no = len(intfs) + 1
+            intf = mininet.link.Intf(intf_name.strip(), node=DummyNode(), port=port_no)
+            intf.port = port_no
+            intfs.append(intf)
+        return intfs
+
+    def flap_interface_ports(self):
+        """Flap all interface ports to trigger start-up behavior"""
+        if self.device_intfs:
+            for device_intf in self.device_intfs:
+                self._flap_interface_port(device_intf.name)
+
+    def _flap_interface_port(self, intf_name):
+        if intf_name.startswith('faux') or intf_name == 'local':
+            LOGGER.info('Flapping device interface %s.', intf_name)
+            self.sec.cmd('ip link set %s down' % intf_name)
+            time.sleep(0.5)
+            self.sec.cmd('ip link set %s up' % intf_name)
+
+    def _create_secondary(self):
+        self.sec_port = int(self.config['ext_port'] if 'ext_port' in self.config else 47)
+        if 'ext_dpid' in self.config:
+            self.sec_dpid = int(self.config['ext_dpid'], 0)
+            self.sec_name = self.config['ext_intf']
+            LOGGER.info('Configuring external secondary with dpid %s on intf %s',
+                        self.sec_dpid, self.sec_name)
+            sec_intf = mininet.link.Intf(self.sec_name, node=DummyNode(), port=1)
+            self.pri.addIntf(sec_intf, port=1)
+        else:
+            self.sec_dpid = 2
+            LOGGER.info('Creating ovs secondary with dpid/port %s/%d',
+                        self.sec_dpid, self.sec_port)
+            self.sec = self.net.addSwitch('sec', dpid=str(self.sec_dpid), cls=self.OVS_CLS)
+
+            link = self.net.addLink(self.pri, self.sec, port1=1,
+                                    port2=self.sec_port, fast=False)
+            LOGGER.info('Added switch link %s <-> %s', link.intf1.name, link.intf2.name)
+            self.sec_name = link.intf2.name
+
+    def is_device_port(self, dpid, port):
+        """Check if the dpid/port combo is for a valid device"""
+        target_dpid = int(self.sec_dpid)
+        return dpid == target_dpid and port < self.sec_port
+
+    def cmd(self, cmd):
+        """Execute the command in the base context"""
+        return self.pri.cmd(cmd)
+
+    def cli(self):
+        """Drop into the mininet CLI"""
+        # pylint: disable=no-member
+        mininet.cli.CLI(self.net)
+
+    def stop(self):
+        """Stop network"""
+        self.net.stop()
+
+    def initialize(self):
+        """Initialize network"""
+
+        LOGGER.debug("Creating miniet...")
+        self.net = mininet.net.Mininet()
+
+        LOGGER.debug("Adding switches...")
+        self.pri = self.net.addSwitch('pri', dpid='1', cls=self.OVS_CLS)
+        self._create_secondary()
+
+        target_ip = "127.0.0.1"
+        LOGGER.debug("Adding controller at %s", target_ip)
+        controller = mininet.node.RemoteController
+        self.net.addController('controller', controller=controller,
+                               ip=target_ip, port=6633)
+
+        LOGGER.info("Starting mininet...")
+        self.net.start()
+
+        if self.sec:
+            self.device_intfs = self._make_device_intfs()
+            for device_intf in self.device_intfs:
+                LOGGER.info("Attaching device interface %s on port %d.",
+                            device_intf.name, device_intf.port)
+                self.sec.addIntf(device_intf, port=device_intf.port)
+                self._switch_attach(self.sec, device_intf)
