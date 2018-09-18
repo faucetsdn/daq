0a8e4e4b24e4559d659d3789b6b17159358ab84b
Trevor Pering <peringknife@google.com>
2018-09-18 12:25:49 -0700

    Fixing uninitialized varibale when gcp not configured

--- daq/gcp.py
+++ daq/gcp.py
@@ -22,17 +22,18 @@ class GcpManager():
         self.config = config
         if 'gcp_cred' not in config:
             LOGGER.info('No gcp_cred credential specified in config')
-            self.pubber = None
+            self._pubber = None
+            self._storage = None
             return
         cred_file = self.config['gcp_cred']
         LOGGER.info('Loading gcp credentials from %s', cred_file)
         # Normal execution assumes default credentials. pylint: disable=protected-access
         (self._credentials, self._project) = google_auth._load_credentials_from_file(cred_file)
         self._client_name = self._parse_creds(cred_file)
-        self.pubber = pubsub_v1.PublisherClient(credentials=self._credentials)
+        self._pubber = pubsub_v1.PublisherClient(credentials=self._credentials)
         LOGGER.info('Initialized gcp pub/sub %s:%s', self._project, self._client_name)
         self.firestore = self._initialize_firestore(cred_file)
-        self._storage_client = storage.Client(project=self._project, credentials=self._credentials)
+        self._storage = storage.Client(project=self._project, credentials=self._credentials)
         self._report_bucket_name = self.REPORT_BUCKET_FORMAT % self._project
         self._ensure_report_bucket()
 
@@ -59,28 +60,31 @@ class GcpManager():
 
     def publish_message(self, topic, message):
         """Publish a message to pub/sub topic"""
-        if not self.pubber:
+        if not self._pubber:
             LOGGER.debug('Ignoring message publish: not configured')
             return
         if 'encode' not in message:
             message = json.dumps(message)
         LOGGER.debug('Sending to topic_path %s/%s: %s', self._project, topic, message)
         #pylint: disable=no-member
-        topic_path = self.pubber.topic_path(self._project, topic)
-        future = self.pubber.publish(topic_path, message.encode('utf-8'),
-                                     projectId=self._project, origin=self._client_name)
+        topic_path = self._pubber.topic_path(self._project, topic)
+        future = self._pubber.publish(topic_path, message.encode('utf-8'),
+                                      projectId=self._project, origin=self._client_name)
         LOGGER.debug('Publish future result %s', future.result())
 
     def _ensure_report_bucket(self):
         bucket_name = self._report_bucket_name
-        if self._storage_client.lookup_bucket(bucket_name):
+        if self._storage.lookup_bucket(bucket_name):
             LOGGER.info('Storage bucket %s already exists', bucket_name)
         else:
             LOGGER.info('Creating storage bucket %s', bucket_name)
-            self._storage_client.create_bucket(bucket_name)
+            self._storage.create_bucket(bucket_name)
 
     def upload_report(self, report_file_name):
         """Uploads a report to a storage bucket."""
-        bucket = self._storage_client.get_bucket(self._report_bucket_name)
+        if not self._storage:
+            LOGGER.info('Ignoring report upload: not configured')
+            return
+        bucket = self._storage.get_bucket(self._report_bucket_name)
         blob = bucket.blob(report_file_name)
         blob.upload_from_filename(report_file_name)
