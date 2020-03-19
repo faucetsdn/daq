367c30093b1e7c40db737c4e3e639c3c67361fa8
Trevor Pering <peringknife@google.com>
2020-03-18 21:36:35 -0700

    Enable some more stuff

--- daq/runner.py
+++ daq/runner.py
@@ -303,9 +303,9 @@ class DAQRunner:
             LOGGER.warning('Target port %d ignored, system not active', target_port)
             self._system_wait += 1
             if self._system_wait > 10:
-                #os.system('ovs-ofctl show pri > ofctl_show_pri.txt')
-                #os.system('ovs-ofctl show sec > ofctl_show_sec.txt')
-                #os.system('ip link > ip_link.txt')
+                os.system('ovs-ofctl show pri > ofctl_show_pri.txt')
+                os.system('ovs-ofctl show sec > ofctl_show_sec.txt')
+                os.system('ip link > ip_link.txt')
                 self.shutdown()
                 raise Exception('exceeded retries')
             return False
