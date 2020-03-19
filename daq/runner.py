59d736fb5fe1a489b14c4a2573637aaaee12adb9
Trevor Pering <peringknife@google.com>
2020-03-18 21:42:18 -0700

    Log error on count exceeded

--- daq/runner.py
+++ daq/runner.py
@@ -303,11 +303,13 @@ class DAQRunner:
             LOGGER.warning('Target port %d ignored, system not active', target_port)
             self._system_wait += 1
             if self._system_wait > 10:
+                LOGGER.error('Wait count exceeded')
+                self._system_wait = 0
                 os.system('ovs-ofctl show pri > ofctl_show_pri.txt')
                 os.system('ovs-ofctl show sec > ofctl_show_sec.txt')
                 os.system('ip link > ip_link.txt')
-                self.shutdown()
-                raise Exception('exceeded retries')
+                #self.shutdown()
+                #raise Exception('exceeded retries')
             return False
 
         if target_port in self.port_targets:
