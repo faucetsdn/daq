4f2e046891d2d4c09ef037e2097602d980787160
Nour <noureddine@saidi.uk>
2020-04-10 16:37:25 +0100

    Increase notes report width
    Pylint fixed

--- daq/report.py
+++ daq/report.py
@@ -176,7 +176,7 @@ class ReportGenerator:
         LOGGER.info('Generating HTML for writing pdf report...')
         pypandoc.convert_file(self.path, 'html',
                               outputfile=self._REPORT_TMP_HTML_PATH,
-                              extra_args=['-V', 'geometry:margin=1.5cm','--columns','1000'])
+                              extra_args=['-V', 'geometry:margin=1.5cm', '--columns', '1000'])
         LOGGER.info('Metamorphosising HTML to PDF...')
         weasyprint.HTML(self._REPORT_TMP_HTML_PATH) \
             .write_pdf(self.path_pdf, stylesheets=[weasyprint.CSS(self._REPORT_CSS_PATH)])
