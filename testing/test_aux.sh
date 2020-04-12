162793b57b2da8ed570f21732b04a7c057b70ca6
Trevor Pering <peringknife@google.com>
2020-04-12 13:11:00 -0700

    Updating error output

--- testing/test_aux.sh
+++ testing/test_aux.sh
@@ -157,6 +157,6 @@ cmd/run -k -s
 
 cat inst/result.log | sort | tee -a $TEST_RESULTS
 find inst/ -name activate.log | sort | tee -a $TEST_RESULTS
-more inst/run-port-*/nodes/*/activate.log | cat
+more inst/run-port-*/nodes/nmap*/activate.log | cat
 
 echo Done with tests | tee -a $TEST_RESULTS
