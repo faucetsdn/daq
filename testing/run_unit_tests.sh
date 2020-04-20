b13f307ba61fed42fe9c59619ad67ebfd005aba6
Anurag Porripireddi <anuragprdi@google.com>
2020-04-20 14:23:53 +0000

    Line br to improve readability

--- testing/run_unit_tests.sh
+++ testing/run_unit_tests.sh
@@ -7,6 +7,12 @@ BASEDIR=`readlink -f $TESTDIR/..`
 cd $BASEDIR || exit 1
 
 $BASEDIR/venv/bin/coverage erase
-PYTHONPATH=$BASEDIR/daq $BASEDIR/venv/bin/coverage run --source $BASEDIR/daq -m unittest discover -s $TESTDIR/unit/ -p "test_*.py"
+
+PYTHONPATH=$BASEDIR/daq $BASEDIR/venv/bin/coverage run \
+    --source $BASEDIR/daq \
+    -m unittest discover \
+    -s $TESTDIR/unit/ \
+    -p "test_*.py"
+
 $BASEDIR/venv/bin/coverage combine || true
 $BASEDIR/venv/bin/coverage report -m
