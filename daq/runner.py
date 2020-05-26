8f7e54aa94ed3dd34ab6c70d4e6659c8827dad0f
Trevor Pering <peringknife@google.com>
2020-05-26 13:39:12 -0700

    Add debug

--- daq/runner.py
+++ daq/runner.py
@@ -742,6 +742,7 @@ class DAQRunner:
         if register:
             self.gcp.register_config(self._RUNNER_CONFIG_PATH, site_config,
                                      self._base_config_changed)
+        LOGGER.info('load_base_config %s %s', base, site_config)
         configurator.merge_config(base, site_config)
         return base
 
