83af174d8d7798402ab293c278467781703cbcf9
Anurag Porripireddi <anuragprdi@google.com>
2020-05-27 19:35:30 +0000

    Test with venv activate

--- testing/run_unit_tests.sh
+++ testing/run_unit_tests.sh
@@ -6,13 +6,15 @@ TESTDIR=`dirname $SCRIPTPATH`
 BASEDIR=`readlink -f $TESTDIR/..`
 cd $BASEDIR
 
+source venv/bin/activate
+
 coverage erase
 
 PYTHONPATH=$BASEDIR/daq:$BASEDIR/mininet:$BASEDIR/faucet:$BASEDIR/forch coverage run \
     --source $BASEDIR/daq \
     -m unittest discover \
     -s $TESTDIR/unit/ \
-    -p "test_*.py"
+    -p "test_*.py" || exit 1
 
 coverage combine || true
 coverage report -m
