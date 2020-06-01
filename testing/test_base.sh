70ad3ba96dde26a7d8eea3985d519e86e476644e
Trevor Pering <peringknife@google.com>
2020-06-01 15:26:08 -0700

    Use ping to test switch config

--- testing/test_base.sh
+++ testing/test_base.sh
@@ -49,7 +49,6 @@ cmd/run -s
 more inst/result.log | tee -a $TEST_RESULTS
 fgrep dp_id inst/faucet.yaml | tee -a $TEST_RESULTS
 fgrep -i switch inst/run-port-02/nodes/ping02/activate.log | tee -a $TEST_RESULTS
-fgrep "Switch test" inst/run-port-02/nodes/switch02/activate.log | tee -a $TEST_RESULTS
 more inst/run-port-02/nodes/ping02/activate.log | cat
 count=$(fgrep icmp_seq=5 inst/run-port-02/nodes/ping02/activate.log | wc -l)
 echo switch ping $count | tee -a $TEST_RESULTS
