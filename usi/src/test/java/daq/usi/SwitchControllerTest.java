84ee3ca2f871e0dfee25f2729483a5a00e2fa4b2
Haoli Du <haolidu@google.com>
2020-06-24 09:43:29 -0700

    stickler is still showing diff lint errors

--- usi/src/test/java/daq/usi/SwitchControllerTest.java
+++ usi/src/test/java/daq/usi/SwitchControllerTest.java
@@ -1,9 +1,8 @@
 package daq.usi;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-import daq.usi.SwitchController;
 import java.util.Map;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -25,21 +24,22 @@ class SwitchControllerTest {
     String[] colNames = {"a", "b"};
     String[] mapNames = {"a", "b"};
     Map<String, String> response = SwitchController.mapSimpleTable(raw, colNames, mapNames);
-    for (String key: response.keySet()) {
+    for (String key : response.keySet()) {
       assertEquals(response.get(key), null);
     }
   }
 
   @Test
   void mapSimpleTableSampleInputAT() {
-    String raw = "Interface   Admin    Pri  Oper     Power Device          Class Max       \n" +
-        "port1.0.1   Enabled  Low  Powered   3337 n/a                 0 15400 [C]";
+    String raw = "Interface   Admin    Pri  Oper     Power Device          Class Max       \n"
+        + "port1.0.1   Enabled  Low  Powered   3337 n/a                 0 15400 [C]";
     String[] colNames = {"Interface", "Admin", "Pri", "Oper", "Power", "Device", "Class", "Max"};
     String[] mapNames = {"interface", "admin", "pri", "oper", "power", "device", "class", "max"};
     Map<String, String> expected = Map.of("interface", "port1.0.1", "admin", "Enabled", "pri",
-        "Low", "oper", "Powered", "power", "3337", "device", "n/a", "class", "0", "max", "15400 [C]");
+        "Low", "oper", "Powered", "power", "3337", "device", "n/a",
+        "class", "0", "max", "15400 [C]");
     Map<String, String> response = SwitchController.mapSimpleTable(raw, colNames, mapNames);
-    for (String key: response.keySet()) {
+    for (String key : response.keySet()) {
       assertEquals(response.get(key), expected.get(key));
     }
   }
