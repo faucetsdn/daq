4cbd034128f9549ebbab4b80a5618a55e323896c
Nour <noureddine@saidi.uk>
2020-07-19 17:14:45 +0100

    fix remove ssh, prevent existing password test from changing

--- testing/test_aux.sh
+++ testing/test_aux.sh
@@ -62,9 +62,9 @@ site_path: inst/test_site
 schema_path: schemas/udmi
 interfaces:
   faux-1:
-    opts: brute broadcast_client ntpv4
+    opts: brute broadcast_client ntpv4 ssh
   faux-2:
-    opts: nobrute expiredtls bacnetfail pubber passwordfail ntpv3 opendns
+    opts: nobrute expiredtls bacnetfail pubber passwordfail ntpv3 opendns ssh
   faux-3:
     opts: tls macoui passwordpass bacnet pubber broadcast_client ssh
 long_dhcp_response_sec: 0
