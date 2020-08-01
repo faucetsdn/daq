51fa03bbe575a9cdcff1c9d35c261c7aa4dbbe92
Francesco Anselmo <francesco.anselmo@gmail.com>
2020-08-01 16:17:16 +0100

    aligning test output with travis-ci output

--- testing/test_base.sh
+++ testing/test_base.sh
@@ -47,7 +47,7 @@ cp config/system/ext.yaml local/system.yaml
 cmd/run -s
 cat inst/result.log | tee -a $TEST_RESULTS
 fgrep dp_id inst/faucet.yaml | tee -a $TEST_RESULTS
-fgrep -i switch inst/run-port-02/nodes/ping02/activate.log | tee -a $TEST_RESULTS
+fgrep -i switch inst/run-port-02/nodes/ping02/activate.log | sed -e "s/\r//g" | tee -a $TEST_RESULTS
 cat -vet inst/run-port-02/nodes/ping02/activate.log
 count=$(fgrep icmp_seq=5 inst/run-port-02/nodes/ping02/activate.log | wc -l)
 echo switch ping $count | tee -a $TEST_RESULTS
