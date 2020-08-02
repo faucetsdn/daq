d12e4ccd13bb5e85709da89d74c047fb97d7640a
Trevor <grafnu@users.noreply.github.com>
2020-08-02 10:23:02 -0700

    GitHub actions (#14)

--- testing/test_aux.sh
+++ testing/test_aux.sh
@@ -122,8 +122,8 @@ sort inst/result.log | tee -a $TEST_RESULTS
 
 # Show partial logs from each test
 head inst/gw*/nodes/gw*/activate.log
-head inst/run-port-*/nodes/*/activate.log
-head inst/run-port-*/nodes/*/tmp/report.txt
+cat inst/run-port-*/nodes/*/activate.log
+cat inst/run-port-*/nodes/*/tmp/report.txt
 ls inst/run-port-01/finish/fail01/ | tee -a $TEST_RESULTS
 
 # Add the port-01 and port-02 module config into the file
