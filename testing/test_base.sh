7f4aa0c96f354f4f22cdd2d0c8543785f0815039
Trevor Pering <peringknife@google.com>
2020-08-17 20:55:07 -0700

    Debugging output

--- testing/test_base.sh
+++ testing/test_base.sh
@@ -58,6 +58,7 @@ cp config/system/alt.yaml local/system.yaml
 timeout 1200s cmd/run -s
 fgrep '9a:02:57:1e:8f:00 learned on vid 1001' inst/cmdrun.log | head -1 | redact | tee -a $TEST_RESULTS
 cat inst/result.log | tee -a $TEST_RESULTS # ping test should fail since there are no dhcp packets captured
+more inst/run-*/nodes/ping*/activate.log | cat
 echo %%%%%%%%%%%%%%%%%%%%%% Mud profile tests | tee -a $TEST_RESULTS
 rm -f local/system.yaml
 cp config/system/muddy.conf local/system.conf
