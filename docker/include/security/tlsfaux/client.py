9562f43c7ed2fe99b709f71db24dafb36fd0da90
jhughesbiot <jonathan.hughes@buildingsiot.com>
2020-09-11 12:17:15 -0700

    formatting

--- docker/include/security/tlsfaux/client.py
+++ docker/include/security/tlsfaux/client.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 import ssl
 import sys
 import time
--- docker/include/security/tlsfaux/generate_certs.py
+++ docker/include/security/tlsfaux/generate_certs.py
@@ -2,46 +2,46 @@ import subprocess, absolute_filepath
 
 filepath = "/".join(absolute_filepath.script_directory) + "/certs/"
 
-#This may be needed, not sure yet, needs more validation testing
+# This may be needed, not sure yet, needs more validation testing
 command_rnd = 'touch /root/.rnd'
 
-#Create the ecparam key to allow for DH/DHE/ECDSA ciphers
+# Create the ecparam key to allow for DH/DHE/ECDSA ciphers
 command_ecparam = 'openssl ecparam -out ' + filepath + 'ecparam.pem -name prime256v1'
 
-#Create a CA Root Key
+# Create a CA Root Key
 command_ca_key = 'openssl genpkey -paramfile ' + filepath + 'ecparam.pem -out ' + filepath + 'CA_Faux.key' 
 
-#Create and self sign the root Certificat e
+# Create and self sign the root Certificat e
 command_ca_crt = 'openssl req -x509 -new -key ' + filepath + 'CA_Faux.key -out ' + filepath + 'CA_Faux.pem -subj "/DC=DAQ/C=US/ST=MountainView/L=KingsX/O=DAQ/OU=Software/CN=DAQ CA Root"'
 
 
 
-#Create the intermediate key
-#command_int_key ='openssl genrsa -out ' + filepath +'intermediate.key 4096'
+# Create the intermediate key
+# command_int_key ='openssl genrsa -out ' + filepath +'intermediate.key 4096'
 
-#Create an intermediate key and csr
-#command_int_key_csr = 'openssl req -nodes -newkey rsa:2048 -keyout ' + filepath + 'intermediate.key -out ' + filepath + 'intermediate.csr -subj "/C=US/ST=MountainView/L=KingsX/O=DAQ/OU=Software/CN=DAQ Intermediate"'
+# Create an intermediate key and csr
+# command_int_key_csr = 'openssl req -nodes -newkey rsa:2048 -keyout ' + filepath + 'intermediate.key -out ' + filepath + 'intermediate.csr -subj "/C=US/ST=MountainView/L=KingsX/O=DAQ/OU=Software/CN=DAQ Intermediate"'
 
-#Generate the intermediate using the csr and sign with the CA Root key
-#command_int_cert = 'openssl x509 -req -in ' + filepath + 'intermediate.csr -CA ' + filepath + 'CA_Faux.pem -CAkey ' + filepath + 'CA_Faux.key -CAcreateserial -out ' + filepath + 'intermediate.pem -days 500 -sha256'
+# Generate the intermediate using the csr and sign with the CA Root key
+# command_int_cert = 'openssl x509 -req -in ' + filepath + 'intermediate.csr -CA ' + filepath + 'CA_Faux.pem -CAkey ' + filepath + 'CA_Faux.key -CAcreateserial -out ' + filepath + 'intermediate.pem -days 500 -sha256'
 
 
 
-#Create a server private key
+# Create a server private key
 command_server_key = 'openssl genpkey -paramfile ' + filepath + 'ecparam.pem -out ' + filepath + 'server.key'  
 
-#Create a server csr
+# Create a server csr
 command_server_csr = 'openssl req -nodes -newkey ec:'+ filepath + 'ecparam.pem -keyout ' + filepath + 'server.key -out ' + filepath + 'server.csr -subj "/C=US/ST=MountainView/L=KingsX/O=DAQ/OU=Software/CN=127.0.0.1"'
 
-#Generate the server cert using the csr and sign with the CA Root key
+# Generate the server cert using the csr and sign with the CA Root key
 command_server_cert = 'openssl x509 -req -in ' + filepath + 'server.csr -CA ' + filepath + 'CA_Faux.pem -CAkey ' + filepath + 'CA_Faux.key -CAcreateserial -out ' + filepath + 'server.crt -days 500 -sha256'
 
-#Generate the certificate using the csr and sign with the Intermediate Root key
-#command_cert = 'openssl x509 -req -in ' + filepath + 'server.csr -CA ' + filepath + 'intermediate.pem -CAkey ' + filepath + 'intermediate.key -CAcreateserial -out ' + filepath + 'server.pem -days 500 -sha256'
+# Generate the certificate using the csr and sign with the Intermediate Root key
+# command_cert = 'openssl x509 -req -in ' + filepath + 'server.csr -CA ' + filepath + 'intermediate.pem -CAkey ' + filepath + 'intermediate.key -CAcreateserial -out ' + filepath + 'server.pem -days 500 -sha256'
 
 
-#Combine the CA and root into one cert
-#command_combine = 'cat ' + filepath + 'server.pem ' + filepath + 'CA_Faux.pem > ' + filepath + 'server.crt'
+# Combine the CA and root into one cert
+# command_combine = 'cat ' + filepath + 'server.pem ' + filepath + 'CA_Faux.pem > ' + filepath + 'server.crt'
 
 
 def run_shell_command(command):
@@ -56,10 +56,6 @@ run_shell_command(command_ecparam)
 
 run_shell_command(command_ca_key)
 run_shell_command(command_ca_crt)
-
-#run_shell_command(command_int_key)
-#run_shell_command(command_int_key_csr)
-#run_shell_command(command_int_cert)
 
 run_shell_command(command_server_key)
 run_shell_command(command_server_csr)
