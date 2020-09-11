8b09d6c4ea24aaa4bfd62946f13ffebeb54c3881
jhughesbiot <jonathan.hughes@buildingsiot.com>
2020-09-11 11:53:17 -0700

    formatting

--- docker/include/security/tlsfaux/client.py
+++ docker/include/security/tlsfaux/client.py
@@ -1,4 +1,6 @@
-import socket, ssl, pprint, absolute_filepath, sys, time
+import ssl
+import sys
+import time
 
 arguments = sys.argv
 HOST = ""
@@ -6,32 +8,31 @@ PORT = 443
 VERSION = arguments[1]
 
 try:
-        HOST=arguments[2]
-except:
-        pass
+  HOST=arguments[2]
+except Exception:
+  pass
 
-SSL_VERSION=None  
-if(VERSION=="1.2"):
-        #Force TLS 1.2
-        SSL_VERSION=ssl.PROTOCOL_TLSv1_2  
-elif(VERSION=="1.3"):
-        #Pick highest version which should be 1.3  
-        SSL_VERSION=ssl.PROTOCOL_TLS 
+SSL_VERSION = None
+if(VERSION == "1.2"):
+  # Force TLS 1.2
+  SSL_VERSION=ssl.PROTOCOL_TLSv1_2  
+elif(VERSION == "1.3"):
+  # Pick highest version which should be 1.3  
+  SSL_VERSION=ssl.PROTOCOL_TLS 
 
 
 print("SSL Client Started...")
-while len(HOST)>0:
-        try:
-                print("Getting Server Certificate...");
-                cert = ssl.get_server_certificate((HOST, PORT), ssl_version=SSL_VERSION)
-                if(len(cert)>0):
-                        print ("Server Certificate Resolved")
-                else:
-                        print("No Certificate Resolved")
-        except Exception as e: 
-                print("Server Certificate Failure")
-                print(e)
-        finally:
-                time.sleep(30)
+while len(HOST) > 0:
+  try:
+    print("Getting Server Certificate...")
+      cert = ssl.get_server_certificate((HOST, PORT), ssl_version=SSL_VERSION)
+        if(len(cert) > 0):
+          print("Server Certificate Resolved")
+        else:
+          print("No Certificate Resolved")
+  except Exception as e:
+    print("Server Certificate Failure")
+    print(e)
+  finally:
+    time.sleep(30)
 print("SSL Client Stopped")
-
