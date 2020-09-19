ee9c420957242200b42d7eb1738c5be548bdcfb1
jhughesbiot <50999916+jhughesbiot@users.noreply.github.com>
2020-09-19 09:46:36 -0700

    Tls updates (#626)
    * TLS test support

---
+++ subset/security/tlstest/src/main/java/CertificateSignatureStatus.java
@@ -0,0 +1,4 @@
+public enum CertificateSignatureStatus {
+  CERTIFICATE_CA_SIGNED,
+  CERTIFICATE_SELF_SIGNED,
+}
--- subset/security/tlstest/src/main/java/Main.java
+++ subset/security/tlstest/src/main/java/Main.java
@@ -1,22 +1,61 @@
 public class Main {
 
   public static void main(String[] args) throws Exception {
+    String tlsReport = "";
+    Report report = new Report();
 
-    if (args.length != 1) {
-      throw new IllegalArgumentException("Expected target ipAddress/hostname as argument");
+    if (args.length != 2) {
+      throw new IllegalArgumentException("Expected 2 args, only " + args.length + " detected.");
     }
-
     String ipAddress = args[0];
+    String caFile = "None".equals(args[1])?null:args[1];
+    System.out.println("Args:");
+    System.out.println("IP Address: " + ipAddress);
+    System.out.println("CA File: " + caFile);
+    //Generate the Client/Server test objects
+    Server tlsServer1_0 = new Server(ipAddress, 443,"1",caFile);
+    Server tlsServer1_2 = new Server(ipAddress, 443,"1.2",caFile);
+    Server tlsServer1_3 = new Server(ipAddress, 443,"1.3",caFile);
+    Client client = new Client(ipAddress,new int[]{443,8883},new String[]{"1.2","1.3"});
+    //Client client_1_3 = new Client(ipAddress,443,"1.3");
+    //Run the tests
+    tlsReport += validateServerTls(tlsServer1_0);
+    tlsReport += validateServerTls(tlsServer1_2);
+    tlsReport += validateServerTls(tlsServer1_3);
+    tlsReport += validateClientTls(client);
+    //tlsReport += validateClientTls(client_1_3);
+    //Generate the results
+    tlsReport += ResultGenerator.generateServerResults(tlsServer1_0);
+    tlsReport += ResultGenerator.generateServerResults(tlsServer1_2);
+    tlsReport += ResultGenerator.generateServerResults(tlsServer1_3);
+    report.writeReport(tlsReport);
+  }
 
-    Certs tlsTest = new Certs(ipAddress, 443);
+  public static String validateServerTls(Server tlsTest) {
+    String report = "";
     try {
-      if (tlsTest.testTLSVersions()) {
-        System.out.println("Certificate read successfully");
-      } else {
-        System.out.println("Certificate read failed");
-      }
-    } catch (Exception e) {
-      System.err.println("Exception main:" + e.getMessage());
+      report += tlsTest.validate();
+    }
+    catch(Exception e){
+      report +="Server TLS failed\n";
+      report +=e.getMessage();
+    }
+    finally {
+      return report;
+    }
+  }
+
+  public static String validateClientTls(Client client){
+    String report = "";
+    try {
+      report += client.validate();
+    }
+    catch(Exception e){
+      report +="Client TLS failed\n";
+      report +=e.getMessage();
+    }
+    finally {
+      return report;
     }
   }
 }
