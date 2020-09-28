3b7b9f69e68459878d98821a10f1adb8e31a40f7
frgitdaq <fr.gitdaq@forestrock.co.uk>
2020-09-28 14:46:45 +0100

    Re implement test 11

--- subset/ipaddr/README.md
+++ subset/ipaddr/README.md
@@ -12,3 +12,9 @@ This is because the functional code included in ipaddr communicates with the ope
 #### Result Cases:
 - PASS: A DHCP request has been received by the device after the port has been disconnected and connected.
 - FAIL: No DHCP request was received (this will also be the case if the target is using a static IP).
+
+### connection.dhcp.ip_change
+- Device communicates after IP change.
+#### Result Cases:
+- PASS: A ping reply is found after the IP address has been changed.
+- FAIL: No ping reply is found after the IP address has been changed.
--- subset/ipaddr/dhcp_tests.py
+++ subset/ipaddr/dhcp_tests.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 import sys
-from scapy.all import DHCP
+from scapy.all import rdpcap, DHCP, ICMP, IP
 
 TEST_REQUEST = str(sys.argv[1])
 DHCP_REQUEST = 3
@@ -9,11 +9,14 @@ DHCP_ACKNOWLEDGE = 5
 def main():
     """main"""
     ipaddr_log = '/tmp/activate.log'
+    scan_file = '/scans/test_ipaddr.pcap'
     report_filename = 'report.txt'
     dash_break_line = '--------------------\n'
     description_dhcp_short = 'Reconnect device and check for DHCP request.'
     description_dhcp_long = 'Wait for lease expiry and check for DHCP request.'
+    description_ip_change = 'Device communicates after IP change.'
     running_port_toggle = 'Running dhcp port_toggle test'
+    running_ip_change = 'Running ip change test'
     ip_notification = 'ip notification'
     result = None
     summary = None
@@ -52,11 +55,40 @@ def main():
         fd.close()
         return 'fail', 'No DHCP request received.'
 
+    def _test_ip_change():
+
+        fd = open(ipaddr_log, 'r')
+        run_ip_change = False
+        ip_change_ip = None
+        for line in fd:
+            if run_ip_change:
+                if ip_notification in line:
+                    ip_change_ip = line.split(ip_notification + ' ')[1].rstrip()
+                    break
+            if running_ip_change in line:
+                run_ip_change = True
+        fd.close()
+
+        if ip_change_ip is None:
+            return 'skip', 'IP change test did not run.'
+
+        capture = rdpcap(scan_file)
+        pingFound = False
+        for packet in capture:
+            if ICMP in packet and packet[IP].src == ip_change_ip:
+                pingFound = True
+        if pingFound:
+            return 'pass', 'Ping response received after IP change.'
+        return 'fail', 'No ping response received after IP change.'
+
     _write_report("{b}{t}\n{b}".format(b=dash_break_line, t=TEST_REQUEST))
 
     if TEST_REQUEST == 'connection.network.dhcp_short':
         result, summary = _test_dhcp_short()
         _write_report("{d}\n{b}".format(b=dash_break_line, d=description_dhcp_short))
+    elif TEST_REQUEST == 'connection.dhcp.ip_change':
+        result, summary = _test_ip_change()
+        _write_report("{d}\n{b}".format(b=dash_break_line, d=description_ip_change))
 
     _write_report("RESULT {r} {t} {s}\n".format(r=result, t=TEST_REQUEST, s=summary))
 
