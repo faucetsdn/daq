4dc7a3eddab0711290658854c1c0680f1d4b7e99
Haoli Du <haolidu@google.com>
2020-10-03 02:48:28 +0000

    stickler

--- daq/external_gateway.py
+++ daq/external_gateway.py
@@ -1,9 +1,11 @@
 """Gateway module for device testing"""
+from __future__ import absolute_import
 
 import logger
 from container_gateway import ContainerGateway
 
 LOGGER = logger.get_logger('external_gateway')
+
 
 class ExternalGateway(ContainerGateway):
     """Gateway collection class for managing testing services"""
