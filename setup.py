4e87130ffde5735d214c70defb5e79c8580028c1
Yufeng Duan <55268016+didovesei@users.noreply.github.com>
2020-10-20 00:47:07 -0700

    lint

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='daq',
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
     pbr=True
