a3f781dcf9c2840112f68e08d1edb7ddb04cac25
Yufeng Duan <55268016+didovesei@users.noreply.github.com>
2020-12-31 09:24:10 -1000

    Update MUD tests (#726)

--- testing/test_base.sh
+++ testing/test_base.sh
@@ -76,35 +76,40 @@ if [ -z `which tcpdump` ]; then
     export PATH=/usr/sbin:$PATH
 fi
 
-device_traffic="tcpdump -en -r inst/run-9a02571e8f01/scans/monitor.pcap port 47808"
-device_bcast="$device_traffic and ether broadcast"
-device_ucast="$device_traffic and ether dst 9a:02:57:1e:8f:02"
-device_xcast="$device_traffic and ether dst 9a:02:57:1e:8f:03"
-cntrlr_traffic="tcpdump -en -r inst/run-9a02571e8f02/scans/monitor.pcap port 47808"
-cntrlr_bcast="$cntrlr_traffic and ether broadcast"
-cntrlr_ucast="$cntrlr_traffic and ether dst 9a:02:57:1e:8f:01"
-cntrlr_xcast="$cntrlr_traffic and ether dst 9a:02:57:1e:8f:03"
+function test_device_traffic {
+    device_num=$1
+    peer_num=$((3-device_num))
+    device_mac=9a:02:57:1e:8f:0$device_num
+    peer_mac=9a:02:57:1e:8f:0$peer_num
+    neighbor_mac=9a:02:57:1e:8f:03
+
+    device_traffic="tcpdump -en -r inst/run-9a02571e8f0$device_num/scans/monitor.pcap port 47808"
+    device_bfr_peer="$device_traffic and ether src $peer_mac and ether broadcast"
+    device_bfr_ngbr="$device_traffic and ether src $neighbor_mac and ether broadcast"
+    device_ufr_peer="$device_traffic and ether src $peer_mac and ether dst $device_mac"
+    device_ufr_ngbr="$device_traffic and ether src $neighbor_mac and ether dst $device_mac"
+    bfr_peer=$($device_bfr_peer | wc -l)
+    bfr_ngbr=$($device_bfr_ngbr | wc -l)
+    ufr_peer=$($device_ufr_peer | wc -l)
+    ufr_ngbr=$($device_ufr_ngbr | wc -l)
+    echo device-$device_num $type $((bfr_peer > 2)) $((bfr_ngbr > 0)) $((ufr_peer > 2)) $((ufr_ngbr > 0)) | tee -a $TEST_RESULTS
+}
 
 function test_mud {
     type=$1
     echo %%%%%%%%%%%%%%%%% test mud profile $type
     cmd/run -s device_specs=resources/device_specs/bacnet_$type.json
+
     echo result $type $(sort inst/result.log) | tee -a $TEST_RESULTS
-    bcast=$($device_bcast | wc -l)
-    ucast=$($device_ucast | wc -l)
-    xcast=$($device_xcast | wc -l)
-    echo device $type $(($bcast > 2)) $(($ucast > 2)) $(($xcast > 0)) | tee -a $TEST_RESULTS
-    bcast=$($cntrlr_bcast | wc -l)
-    ucast=$($cntrlr_ucast | wc -l)
-    xcast=$($cntrlr_xcast | wc -l)
-    echo cntrlr $type $(($bcast > 2)) $(($ucast > 2)) $(($xcast > 0)) | tee -a $TEST_RESULTS
+
+    test_device_traffic 1
+    test_device_traffic 2
+
     more inst/run-*/nodes/*/activate.log | cat
 }
 
 test_mud open
-test_mud base
 test_mud todev
-test_mud frdev
 test_mud none
 test_mud star
 
