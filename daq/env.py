ca2c2f30b746c198308fdb37b0d304b0d919a6f4
Trevor Pering <peringknife@google.com>
2021-02-11 17:16:32 -0800

    Update logging

--- daq/env.py
+++ daq/env.py
@@ -1,11 +1,16 @@
 """DAQ installion environment"""
+
 from __future__ import absolute_import
+import logger
 import os
 
-def os_getenv(key):
+LOGGER = logger.get_logger('env')
+
+def _os_getenv(key):
     var = os.getenv(key)
+    LOGGER.info('%s is %s', key, var)
     assert var, '%s not defined in environment' % key
 
-DAQ_LIB_DIR = os_getenv('DAQ_LIB')
-DAQ_CONF_DIR = os_getenv('DAQ_CONF')
-DAQ_RUN_DIR = os_getenv('DAQ_RUN')
+DAQ_LIB_DIR = _os_getenv('DAQ_LIB')
+DAQ_CONF_DIR = _os_getenv('DAQ_CONF')
+DAQ_RUN_DIR = _os_getenv('DAQ_RUN')
