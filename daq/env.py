1766b7e54400940e3c73a1a58d4cb9bd8c8db3cc
Trevor Pering <peringknife@google.com>
2021-02-11 16:20:17 -0800

    Return env

--- daq/env.py
+++ daq/env.py
@@ -5,6 +5,7 @@ import os
 def os_getenv(key):
     var = os.getenv(key)
     assert var, '%s not defined in environment' % key
+    return var
 
 DAQ_LIB_DIR = os_getenv('DAQ_LIB')
 DAQ_CONF_DIR = os_getenv('DAQ_CONF')
