8560e30b280196f2df34cedac375d8f285438aaf
Trevor Pering <peringknife@google.com>
2021-02-11 19:21:38 -0800

    Fix lint

--- daq/env.py
+++ daq/env.py
@@ -1,16 +1,19 @@
 """DAQ installion environment"""
 
 from __future__ import absolute_import
-import logger
 import os
 
+import logger
+
 LOGGER = logger.get_logger('env')
+
 
 def _os_getenv(key):
     var = os.getenv(key)
     LOGGER.info('%s is %s', key, var)
     assert var, '%s not defined in environment' % key
     return var
+
 
 DAQ_LIB_DIR = _os_getenv('DAQ_LIB')
 DAQ_CONF_DIR = _os_getenv('DAQ_CONF')
