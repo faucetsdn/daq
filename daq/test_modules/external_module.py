0a8def68c93955db02b2a18607cfa3b0ac76ef28
Trevor Pering <peringknife@google.com>
2021-02-17 18:54:47 -0800

    Fix check

--- daq/test_modules/external_module.py
+++ daq/test_modules/external_module.py
@@ -124,9 +124,9 @@ class ExternalModule(HostModule):
 
     def _map_if_exists(self, params, kind):
         base = params.get('%s_base' % kind)
-        abs_base = os.path.abspath(base)
-        LOGGER.info('TAP checking %s %s', kind, abs_base)
+        LOGGER.info('TAP checking %s %s', kind, base)
         if base and os.path.exists(base):
+            abs_base = os.path.abspath(base)
             dst = os.path.join(self.basedir, 'config', kind)
             LOGGER.info('%s mapping %s to %s', self, abs_base, dst)
             return (abs_base, dst)
