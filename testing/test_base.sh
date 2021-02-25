2bc2faea3bc372f8edb9a31dec33bfacec0d4ba3
Anurag Porripireddi <anuragprdi@google.com>
2021-02-25 01:23:48 +0000

    Try without docker force

--- testing/test_base.sh
+++ testing/test_base.sh
@@ -35,8 +35,8 @@ cat inst/result.log | tee -a $TEST_RESULTS
 
 echo %%%%%%%%%%%%%%%%%%%%%% Telnet fail | tee -a $TEST_RESULTS
 # Check that an open port causes the appropriate failure.
-docker ps
-docker rmi -f daqf/test_hold:latest # Check case of missing image
+docker ps -a
+docker rmi daqf/test_hold:latest # Check case of missing image
 cmd/run -s -k interfaces.faux.opts=telnet
 echo DAQ result code $? | tee -a $TEST_RESULTS
 cat inst/result.log | tee -a $TEST_RESULTS
