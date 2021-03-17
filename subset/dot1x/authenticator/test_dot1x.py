bbdbcae53006a6be027e3cf0efcdb81ee048e9e2
Anurag Porripireddi <anuragprdi@google.com>
2021-03-17 22:47:33 +0000

    Use main

--- subset/dot1x/authenticator/test_dot1x.py
+++ subset/dot1x/authenticator/test_dot1x.py
@@ -1,7 +1,12 @@
 import sys
 
-write_file = sys.argv[1]
-print('Writing to file %s' % write_file)
-result ='Authentication for <mac> successful.'
-with open(write_file,  'w') as w_file:
-    w_file.write(result)
+
+def main():
+    write_file = sys.argv[1]
+    result ='Authentication for <mac> successful.'
+    with open(write_file,  'w') as w_file:
+        w_file.write(result)
+
+
+if __name__ == '__main__':
+    main()
