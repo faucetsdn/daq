16512605e2a48a4302b156426a6d9ec120cb579d
Anurag Porripireddi <anuragprdi@google.com>
2021-03-25 19:41:48 -0700

    Revert "Acl changes for EAP (#801)" (#806)

--- subset/dot1x/authenticator/utils.py
+++ subset/dot1x/authenticator/utils.py
@@ -8,19 +8,11 @@ def get_logger(logname):
     """Create and return a logger object."""
     logger = logging.getLogger(logname)
     logger.setLevel(logging.INFO)
-
     stdout_handler = logging.StreamHandler(sys.stdout)
     stdout_handler.setLevel(logging.INFO)
     stdout_handler.setFormatter(
         logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
     logger.addHandler(stdout_handler)
-
-    logfile_handler = logging.FileHandler('/tmp/dot1x_debug_log')
-    logfile_handler.setLevel(logging.DEBUG)
-    logfile_handler.setFormatter(
-        logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-    logger.addHandler(logfile_handler)
-
     return logger
 
 
