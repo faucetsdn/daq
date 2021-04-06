3ac448c1b7dd8794dfd78da9e4d800b873810516
Anurag Porripireddi <anuragprdi@google.com>
2021-04-06 19:46:34 +0000

    Increase timeout time

--- subset/dot1x/authenticator/authenticator.py
+++ subset/dot1x/authenticator/authenticator.py
@@ -143,7 +143,7 @@ class Authenticator:
     IDLE_TIME = 9
     RETRY_COUNT = 3
     RADIUS_PORT = 1812
-    EAPOL_IDLE_TIME = 100
+    EAPOL_IDLE_TIME = 180
 
     def __init__(self, config_file):
         self.state_machines = {}
