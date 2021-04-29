581a1a0980809a12f991abc63ccf00b90419a155
Anurag Porripireddi <anuragprdi@google.com>
2021-04-29 20:50:56 +0000

    Disable other tests

--- testing/test_modules.sh
+++ testing/test_modules.sh
@@ -12,23 +12,22 @@ if [ -d venv ]; then
     source venv/bin/activate
 fi
 
-TLS_CONFIG_DIR=inst/modules/tls/config
-echo Resolve module config to $TLS_CONFIG_DIR
-mkdir -p $TLS_CONFIG_DIR
-python3 daq/configurator.py --json \
-    resources/test_site/site_config.json > $TLS_CONFIG_DIR/module_config.json
-
-TEST_LIST=/tmp/module_tests.txt
 # TODO: Enable TLS tests once fixed
+# TLS_CONFIG_DIR=inst/modules/tls/config
+# echo Resolve module config to $TLS_CONFIG_DIR
+# mkdir -p $TLS_CONFIG_DIR
+# python3 daq/configurator.py --json \
+#    resources/test_site/site_config.json > $TLS_CONFIG_DIR/module_config.json
+
+# TEST_LIST=/tmp/module_tests.txt
 # cat > $TEST_LIST <<EOF
 # tls alt
 # tls alt tls
 # tls alt expiredtls
-cat > $TEST_LIST <<EOF
-ssh
-ssh ssh
-ssh sshv1
-EOF
+# ssh
+# ssh ssh
+# ssh sshv1
+# EOF
 
 DAQ_TARGETS=aardvark,aardvark2,faux1,faux2 cmd/build missing inline
 
