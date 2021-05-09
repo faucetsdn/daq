024cda1d56fbcbb5bd10ec5f648c5134f6995c19
Trevor Pering <peringknife@google.com>
2021-05-08 22:28:15 -0700

    FIlter

--- daq/runner.py
+++ daq/runner.py
@@ -1001,8 +1001,9 @@ class DAQRunner:
 
     def _calculate_device_result(self, device, test_results):
         failed = False
-        module_results = test_results.get('modules', {}).values()
-        for module_result in module_results:
+        processed = set()
+        module_results = test_results.get('modules', {}).items()
+        for module_key, module_result in module_results:
             exception = module_result.get(report.ResultType.EXCEPTION.value)
             if exception:
                 LOGGER.warning('Failing run with exception: %s', exception)
@@ -1012,6 +1013,8 @@ class DAQRunner:
             if return_code:
                 LOGGER.warning('Failing run with return code %s', return_code)
                 failed = True
+            if return_code is not None:
+                processed.add(module_key)
 
             module_tests = module_result.get('tests', {})
             for test_name, test_result in module_tests.items():
@@ -1020,11 +1023,10 @@ class DAQRunner:
                 if result not in ('pass', 'skip'):
                     failed = True
 
-        if len(device.host.enabled_tests) != len(module_results):
-            LOGGER.info('module results %s', module_results)
-            LOGGER.info('enabled modules %s', device.host.enabled_tests)
+        LOGGER.info('processed results %s', processed)
+        if len(device.host.enabled_tests) != len(processed):
             LOGGER.info('%s report had %s out of expected %s modules', device.mac,
-                        len(module_results), len(device.host.enabled_tests))
+                        len(processed), len(device.host.enabled_tests))
             failed = True
 
         return PortBehavior.failed if failed else PortBehavior.passed
