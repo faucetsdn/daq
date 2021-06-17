1988decf1205b49510fa29d444ae1d962d4c2266
Trevor Pering <peringknife@google.com>
2021-06-17 16:59:32 -0700

    Fix arpscan

--- daq/base_gateway.py
+++ daq/base_gateway.py
@@ -276,11 +276,12 @@ class BaseGateway(ABC):
             address = next(subnet.hosts())
             log_file = os.path.join(self.tmpdir, str(subnet).replace('/', '_'))
             log_fd = open(log_file, 'w')
+            LOGGER.info('Scanning subnet %s from %s for %s', subnet, address, mac)
             host.cmd('ip addr add %s dev %s' % (str(address), intf))
-            active_pipe = host.popen(cmd % (intf, mac,
-                                            '' if vid is None else '--vlan=%s' % vid,
-                                            str(subnet)),
-                                     stdin=DEVNULL, stdout=PIPE, env=os.environ)
+            vid_opt = '' # if vid is None else '--vlan=%s' % vid
+            full_cmd = cmd % (intf, mac, vid_opt, str(subnet))
+            LOGGER.info('arp-scan command: %s', full_cmd)
+            active_pipe = host.popen(full_cmd, stdin=DEVNULL, stdout=PIPE, env=os.environ)
             self.runner.monitor_stream(self.name, active_pipe.stdout, copy_to=log_fd,
                                        hangup=lambda: self._discover_host_hangup_callback(
                                            mac, log_fd, log_file, hangup_callback_callback))
