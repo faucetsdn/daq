ca83170a23a51cd6a960d66d09cbdcdc01049931
Trevor Pering <peringknife@google.com>
2021-08-10 10:56:13 -0700

    Add some more unit tests

--- testing/unit/test_udmi_manager.py
+++ testing/unit/test_udmi_manager.py
@@ -10,10 +10,12 @@ from runner import Device
 
 from udmi.agent.mqtt_manager import MqttManager
 
+from proto.report_pb2 import DeviceReport
+
 class UdmiDiscoverBase(unittest.TestCase):
     """Base tests for UDMI discovery"""
 
-    BASE_CONFIG = {
+    UDMI_CONFIG = {
         'cloud_config': {
             'cloudRegion': 'us_west1',
             'project_id': 'hello'
@@ -26,14 +28,13 @@ class UdmiDiscoverBase(unittest.TestCase):
 
     def setUp(self):
         self._mqtt = None
-        print('Config is %s', self.BASE_CONFIG)
-        self._udmi = UdmiManager(self.BASE_CONFIG, mqtt_factory=self._mock_mqtt_manager)
+        self._udmi = UdmiManager(self.UDMI_CONFIG, mqtt_factory=self._mock_mqtt_manager)
 
 
-class UdmiDiscoverTest(UdmiDiscoverBase):
+class UdmiDiscoveryTest(UdmiDiscoverBase):
     """Basic discovery tests"""
 
-    def test_basic_send(self):
+    def test_discovery_send(self):
         """Test that sending works when there's something to send"""
         device = Device()
         self._udmi.discovery(device)
@@ -46,9 +47,18 @@ class UdmiDiscoverTest(UdmiDiscoverBase):
 class UdmiEmptyConfigTest(UdmiDiscoverBase):
     """Base test for empty configiration"""
 
-    BASE_CONFIG = copy.deepcopy(UdmiDiscoverBase.BASE_CONFIG)
-    del BASE_CONFIG['cloud_config']['project_id']
+    # Create a copy config that doesn't have project_id defined.
+    UDMI_CONFIG = copy.deepcopy(UdmiDiscoverBase.UDMI_CONFIG)
+    del UDMI_CONFIG['cloud_config']['project_id']
 
     def test_no_manager(self):
         """Test the case when there is an empty configuraiton"""
         self.assertIsNone(self._mqtt)
+
+        # These calls should simply not throw an exception in this case.
+        device = Device()
+        device.mac = '123435'
+        self._udmi.discovery(device)
+
+        report = DeviceReport()
+        self._udmi.report(report)
