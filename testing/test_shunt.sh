26e7f64ea3fbbdc95fcbbf0bffe3c39881e57611
Anurag Porripireddi <anuragprdi@google.com>
2021-08-26 01:59:37 +0000

    Adds test_shunt to create two docker containers and test ping

---
+++ testing/test_shunt.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+source etc/FILES_MAPPING
+
+mkdir -p out
+test_script=${0##*/}
+def_name=${test_script%.sh}.out
+TEST_RESULTS=${TEST_RESULTS:-out/$def_name}
+GOLDEN_FILE=testing/$def_name
+
+echo Shunt Test > $TEST_RESULTS
+
+docker-compose -f ./shunt/docker-compose.yaml up --build -d
+docker exec shunt_host1_1 ping -c 1 192.168.1.21
+
+if [ $? -eq 0 ]; then
+    echo Ping succesful | tee -a $TEST_RESULTS
+else
+    echo Ping unsuccesful | tee -a $TEST_RESULTS
+fi
+
+docker-compose -f ./shunt/docker-compose.yaml down
+
+echo Done with tests | tee -a $TEST_RESULTS
+
+set -o pipefail
+exit_code=0
+echo
+echo Test results $TEST_RESULTS
+cat $TEST_RESULTS
+echo
+echo Comparing $GOLDEN_FILE against $TEST_RESULTS
+diff $GOLDEN_FILE $TEST_RESULTS | cat -vet || exit_code=1
+echo
+
+echo Done with tests.
+exit $exit_code
