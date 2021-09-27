7f033f823295472df63e1eb4b193d61ff9d98def
Trevor Pering <peringknife@google.com>
2021-09-27 11:57:56 -0700

    Fix path cleanup

--- daq/runner.py
+++ daq/runner.py
@@ -245,8 +245,10 @@ class DAQRunner:
         if os.path.isdir(report.REPORT_BASE_DIR):
             shutil.rmtree(report.REPORT_BASE_DIR, ignore_errors=True)
         for path in os.listdir(DAQ_RUN_DIR):
-            if path.startswith(connected_host.DEV_DIR_PREFIX) and os.path.isdir(path):
-                shutil.rmtree(path, ignore_errors=True)
+            fullpath = os.path.join(DAQ_RUN_DIR, path)
+            if path.startswith(connected_host.DEV_DIR_PREFIX) and os.path.isdir(fullpath):
+                LOGGER.info('Removing %s', fullpath)
+                shutil.rmtree(fullpath, ignore_errors=True)
 
     def _init_device_result_handler(self):
         server_port = self.config.get('device_reporting', {}).get('server_port')
