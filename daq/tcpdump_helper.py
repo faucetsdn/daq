5d91f084638b822007df984c8b69b107a8aae9db
henry54809 <haolidu@google.com>
2021-10-26 16:00:20 -0400

    append LIB path for catwrap (#928)
    Change-Id: Icbcd67bb3352e8b39f56340693e1e861892c80f8

--- daq/tcpdump_helper.py
+++ daq/tcpdump_helper.py
@@ -10,6 +10,8 @@ import subprocess
 import threading
 import logger
 
+from env import DAQ_LIB_DIR
+
 DEVNULL = open(os.devnull, 'wb')
 
 LOGGER = logger.get_logger('tcpdump')
@@ -50,7 +52,11 @@ class TcpdumpHelper:
             self.intf_name, tcpdump_flags, tcpdump_filter)
 
         pipe_cmd = timeout_soft_cmd(tcpdump_cmd, timeout)
-        pipe_cmd = 'bin/catwrap ' + pipe_cmd
+        pipe_cmd_wrapper = os.path.join('bin', 'catwrap')
+        # When running container gateway, catwrap is under bin/
+        if not os.path.isfile(pipe_cmd_wrapper):
+            pipe_cmd_wrapper = os.path.join(DAQ_LIB_DIR, pipe_cmd_wrapper)
+        pipe_cmd = '%s %s' % (pipe_cmd_wrapper, pipe_cmd)
 
         LOGGER.debug(pipe_cmd)
 
