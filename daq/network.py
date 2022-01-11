22a7ae4b29b75c1c5b886859ef5b335c4ada853f
Anurag Porripireddi <anuragprdi@google.com>
2022-01-11 01:40:49 +0000

    Debug Reset

--- daq/network.py
+++ daq/network.py
@@ -72,8 +72,8 @@ class TestNetwork:
         self._used_ip_indices = set()
         self.topology = FaucetTopology(self.config)
         switch_setup = config.get('switch_setup', {})
-        self.ext_intf = switch_setup.get('data_intf') if switch_setup.get('auto_session') else None
-        LOGGER.info('Debug ext_intf: %s data_intf: %s of_dpid: %s', self.ext_intf, switch_setup.get('data_intf'), switch_setup.get('of_dpid'))
+        self.ext_intf = switch_setup.get('data_intf')
+        LOGGER.info('Debug ext_intf: %s data_intf: %s auto_session: %s', self.ext_intf, switch_setup.get('data_intf'), switch_setup.get('auto_session'))
         self.ext_mac = switch_setup.get('data_mac')
         self.ext_faucet_ofpt = int(switch_setup.get('lo_port', self.DEFAULT_FAUCET_OF_PORT))
         self.ext_gauge_ofpt = int(switch_setup.get('lo_port_2', self.DEFAULT_GAUGE_OF_PORT))
