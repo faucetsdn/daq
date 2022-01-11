09c491d50c9c4af3e88e340236cb148775fe98b3
Anurag Porripireddi <anuragprdi@google.com>
2022-01-11 08:34:13 +0000

    Debug

--- daq/network.py
+++ daq/network.py
@@ -72,7 +72,8 @@ class TestNetwork:
         self._used_ip_indices = set()
         self.topology = FaucetTopology(self.config)
         switch_setup = config.get('switch_setup', {})
-        if self.config.get('run_trigger', {}).get('auto_session'):
+        #if self.config.get('run_trigger', {}).get('auto_session'):
+        if switch_setup.get('model')  == 'NON_OVS':
             self.ext_intf = None
         else:
             self.ext_intf = switch_setup.get('data_intf')
